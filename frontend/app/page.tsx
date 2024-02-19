407a83dc74eb01e0d7eadbcd6a465be57c48cad7
Elsie Oduor <eoatieno@gmail.com>
2024-02-19 17:18:49 +0300

    Added app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/shared/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h2>Wagwaan</h2>
+    <Hero />
+    </>
+  );
+}
