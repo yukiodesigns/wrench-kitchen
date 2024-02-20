04c188b15642e0483b07370c5f34c83f8f64d2af
yuki designs <yukiosdesigns@gmail.com>
2024-02-20 13:55:19 +0300

    Added component

--- frontend/components/shared/Hero.tsx
+++ frontend/components/shared/Hero.tsx
@@ -1,23 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
     <div>
       <header>
-        <div className="relative overflow-hidden bg-cover bg-no-repeat" style={{backgroundPosition: '50%', backgroundImage: "url('https://tecdn.b-cdn.net/img/new/slides/146.webp')", height: '350px'}}>
-          <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed" style={{backgroundColor: 'rgba(0, 0, 0, 0.75)'}}>
+        <div className="relative overflow-hidden bg-cover bg-no-repeat" style={{backgroundPosition: '50%', backgroundImage: "url('/imgs/pexels-brigitte-tohm-239581.jpg')", height: '350px'}}>
+          <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed" style={{backgroundColor: 'rgba(0, 0, 0, 0.55)'}}>
             <div className="flex h-full items-center justify-center">
               <div className="px-6 text-center text-white md:px-12">
                 <h1 className="mb-6 text-5xl font-bold">Flavours of India</h1>
                 <h3 className="mb-8 text-xl font-bold">We are ready to serve you.</h3>
-                {/* <button
-                  type="button"
-                  className="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                  data-te-ripple-init
-                  data-te-ripple-color="light">
-                  Get started
-                </button> */}
-                
+               
+                <div className="flex justify-center">
+                  <div className="mr-6 ">
+                    <Image src="/icons/chef.png" alt='chef' width={50} height={50} className="bg-white rounded-full p-1"/>
+                  </div>
+                  <div className="mr-6">
+                    <Image src="/icons/chef.png" alt='chef' width={50} height={50} className="bg-white rounded-full p-1"/>
+                  </div>
+                  <div>
+                    <Image src="/icons/chef.png" alt='chef' width={50} height={50} className="bg-white rounded-full p-1"/>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
