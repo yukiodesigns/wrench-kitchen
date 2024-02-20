71f3ce838fcb5a3f04a38a0622af14d4cc93c968
yuki designs <yukiosdesigns@gmail.com>
2024-02-20 11:30:21 +0300

    Modified Hero

--- frontend/components/shared/Hero.tsx
+++ frontend/components/shared/Hero.tsx
@@ -10,13 +10,14 @@ const Hero = () => {
               <div className="px-6 text-center text-white md:px-12">
                 <h1 className="mb-6 text-5xl font-bold">Flavours of India</h1>
                 <h3 className="mb-8 text-xl font-bold">We are ready to serve you.</h3>
-                <button
+                {/* <button
                   type="button"
                   className="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                   data-te-ripple-init
                   data-te-ripple-color="light">
                   Get started
-                </button>
+                </button> */}
+                
               </div>
             </div>
           </div>
