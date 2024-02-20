7e014cd81a6b0d4a44bb81692915e116a9959378
yuki designs <yukiosdesigns@gmail.com>
2024-02-20 13:55:58 +0300

    Prgress

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,8 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        {children}</body>
+        {children}
         <Footer />
+        </body>
+       
     </html>
   );
 }
