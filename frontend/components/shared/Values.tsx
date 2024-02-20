a9cc0a72a63529783202201163c3ae749dcfaf96
yuki designs <yukiosdesigns@gmail.com>
2024-02-20 11:29:54 +0300

    Created values

---
+++ frontend/components/shared/Values.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MdVerified } from "react-icons/md";
+import { FaThumbsUp } from "react-icons/fa";
+
+const Values = () => {
+  return (
+    <div>Values</div>
+  )
+}
+
+export default Values
