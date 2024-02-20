56da22269344edaed8e3c778493a5019779b13b3
yuki designs <yukiosdesigns@gmail.com>
2024-02-20 13:58:17 +0300

    Progress

--- frontend/components/shared/Footer.tsx
+++ frontend/components/shared/Footer.tsx
@@ -14,17 +14,21 @@ const Footer = () => {
           </div>
           
           {/* Second section */}
-          <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-4 flex items-center">
+          <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-4">
             {/* Google Maps iframe */}
             <iframe
-                className="w-full h-40 md:h-64 rounded-lg mr-10"
+                className="w-full h-40 md:h-64 rounded-lg"
                 loading="lazy"
                 allowFullScreen
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.1436937483996!2d36.82194661477156!3d-1.2865644990672618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f182a5b3d73ad%3A0x4b48fc9c34bf95c2!2sNairobi%2C%20Kenya!5e0!3m2!1sen!2s!4v1644938608622!5m2!1sen!2s"
                 title="Map of Nairobi"
             ></iframe>
-            {/* Contact details with icons */}
-            <div>
+           </div>
+          
+          {/* Third section */}
+          <div className="w-full md:w-auto">
+            <div className="mb-4 md:mb-0 md:mr-4">
+              {/* Contact details with icons */}
               <div className="flex items-center mb-2 ">
                 <Image src="/icons/maps-and-flags.png" alt='map' width={20} height={20} className='mr-4'/>
                 <p>Nairobi, Kenya</p>
@@ -35,7 +39,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center">
                 <Image src="/icons/phone-call.png" alt='map' width={20} height={20} className='mr-4'/>
-                <p>+25494843163</p>
+                <p>+254 794 843163</p>
               </div>
             </div>
           </div>
