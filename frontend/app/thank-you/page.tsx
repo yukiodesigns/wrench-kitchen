b93e35a20a068e7d8210d09bc80d3832f5946706
yuki designs <yukiosdesigns@gmail.com>
2024-04-26 13:45:08 +0300

    Made thanks page

--- frontend/app/thank-you/page.tsx
+++ frontend/app/thank-you/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const Thanks = () => {
+const ThankYouPage = () => {
   return (
-    <div>Thank You</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-semibold mb-8">Thank You!</h1>
+      <p className="text-lg mb-8">Your order has been successfully placed.</p>
+      <Link href="/shop">
+        {/* <a className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 ease-in-out"> */}
+          Back to Shop
+        {/* </a> */}
+      </Link>
+    </div>
+  );
+};
 
-export default Thanks
+export default ThankYouPage;
